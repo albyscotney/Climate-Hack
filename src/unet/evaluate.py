ecf22a697412a9f22a43d239e16faee7cb5079d8
albyscotney <77618707+albyscotney@users.noreply.github.com>
2022-06-04 16:16:28 +0100

    Add files via upload

---
+++ src/unet/evaluate.py
@@ -0,0 +1,57 @@
+import numpy as np
+import torch
+
+from climatehack import BaseEvaluator
+from model import UNetPPDropoutMultiBigBoi
+
+
+class Evaluator(BaseEvaluator):
+    def setup(self):
+        """Sets up anything required for evaluation.
+
+        In this case, it loads the trained model (in evaluation mode)."""
+
+        #preprocess = ImagesPreprocess()
+        
+        #self.model = PerceiverCH(preprocess, latent_dim=(32, 128), heads=8, wide_factor=4, latent_count=6)
+        self.model = UNetPPDropoutMultiBigBoi()
+        self.model.load_state_dict(torch.load("model", map_location='cpu'))
+        self.model.eval()
+
+    def predict(self, coordinates: np.ndarray, data: np.ndarray) -> np.ndarray:
+        """Makes a prediction for the next two hours of satellite imagery.
+
+        Args:
+            coordinates (np.ndarray): the OSGB x and y coordinates (2, 128, 128)
+            data (np.ndarray): an array of 12 128*128 satellite images (12, 128, 128)
+
+        Returns:
+            np.ndarray: an array of 24 64*64 satellite image predictions (24, 64, 64)
+        """
+
+        assert coordinates.shape == (2, 128, 128)
+        assert data.shape == (12, 128, 128)
+
+        with torch.no_grad():
+            prediction = (
+                self.model(torch.from_numpy(data).view(1, 12, 128, 128).float())
+                .view(24, 64, 64)
+                .detach()
+                .numpy()
+            )
+
+            assert prediction.shape == (24, 64, 64)
+            prediction[12:24] = np.rint(prediction[12:24])
+
+            return prediction
+
+
+def main():
+    evaluator = Evaluator()
+    evaluator.evaluate()
+
+
+if __name__ == "__main__":
+    main()
+
+# Playing with other models, credit to pmistry9597
